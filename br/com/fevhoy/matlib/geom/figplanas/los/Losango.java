73263a4402be84e678e4949ef8eee1e83df7fa06
Peixe3d <mateus.b.s.terra@gmail.com>
2015-12-19 00:09:51 -0200

    v1

--- br/com/fevhoy/matlib/geom/figplanas/los/Losango.java
+++ br/com/fevhoy/matlib/geom/figplanas/los/Losango.java
@@ -1,34 +1,27 @@
 package br.com.fevhoy.matlib.geom.figplanas.los;
 
+import br.com.fevhoy.matlib.geom.figplanas.tria.Triangulo;
 
 /**
  * Esse programa calculara a area, perimetro e lados de um losango baseado em suas diagonais
  * 
  * @author Rafael Mascarenhas Dal Moro
  * @version 1.0 1/1/2015
- * review <name> <date>
+ * review Mateus Berardo 18/12/2015
  */
 public class Losango extends FigPlana{
     // metodo "getArea" para area do losango
     public double getArea( double d, double d2){
-		double D=d;
-		double D2=d2;
-		double A= D * D2 / 2;
-		return A;
+		return d*d2/2;
     }
     // metodo "getLado" para lado do losango
-    public double getLado(double d, double d2, boolean dif){
-		double D=d;
-		double D2=d2;
-		double L= Math.sqrt( Math.pow(D,2)/2 + Math.pow(D2,2)/2); 
-     return L;
+    public double getLado(double d, double d2){
+		return Triangulo.pitagoras(d/2, d2/2);
     }
     // metodo"getPer" para perimetro do losango
-    public double getPer(boolean semi, double l1, double l2){
-     double[] LS = {l1,l1,l2,l2};
-	 getPerimetro(LS,semi);
-	 return getPerimetro();
+    public double getPerimetro(double l1, double l2, boolean semi){
+     	double[] LS = {l1,l1,l2,l2};
+	return getPerimetro(LS,semi);
     }
-    
 }
     
